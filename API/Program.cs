36052057b71b8b8efca0ac924a0461fdd687b5d0
BernardoModerno <bernardo.moderno@gmail.com>
2020-07-06 10:35:54 -0300

    Configurações Iniciais

---
+++ API/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
+
+/*Comandos Utilizados
+dotnet new sln
+dotnet new webapi -o API
+dotnet sln add API
+dotnet sln list
+
+
+
+*/
