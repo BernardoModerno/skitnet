6472e57e138fca973b2fc00d243263c251530797
BernardoModerno <bernardo.moderno@gmail.com>
2020-07-22 21:08:46 -0300

    Criando e referenciando outros projetos.

--- API/Program.cs
+++ API/Program.cs
@@ -34,5 +34,19 @@ foi adicionado o microsoft.entityframeworkcore.design no nuget
 dotnet ef migrations add InicialCreate -o Data/Migrations
 dotnet ef database update
 dotnet watch run
+dotnet new classlib -o Core
+dotnet new classlib -o Infrastructure
+dotnet sln add Core
+dotnet sln add Infrastructure
+cd API
+dotnet add reference ../Infrastructure
+cd ..
+cd Infrastructure
+dotnet add reference ../Core
+cd ..
+dotnet restore
+dotnet build
+cd API
+dotnet watch run
 
 */
